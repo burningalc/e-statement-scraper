14288059bf79b82b18ff7be3688a5ea3eaf81deb
burningalc <45654846+burningalc@users.noreply.github.com>
2020-05-07 22:23:54 +0800

    American Express completed update

---
+++ HangSeng.py
@@ -0,0 +1,5 @@
+from bs4 import BeautifulSoup
+import requests
+from credential import credential
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
--- americanExpress.py
+++ americanExpress.py
@@ -1,26 +1,51 @@
+# import library
 from bs4 import BeautifulSoup
 import requests
-from credential import credential
 from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.chrome.options import Options
+from datetime import datetime
 
-chrome_options = Options()
-chrome_options.add_argument('--headless')
-chrome_options.add_argument('--disable-gpu')
+# import credential
+from credential import credential
 
-chromeDriver = "chromedriver.exe"
-driver = webdriver.Chrome(chromeDriver)
+# setup chromedriver
+chrome = "drivers/chromedriver.exe"
 
+# options
+chromeOptions = webdriver.ChromeOptions()
+chromeOptions.add_argument("incognito")
+# chromeOptions.add_argument("headless")
+prefs = {
+    "profile.default_content_settings.popups": 0,
+    "download.default_directory": "C:\\Users\\Kenny\\Documents\\E-statement\\AE"
+}
+chromeOptions.add_experimental_option('prefs', prefs)
+
+driver = webdriver.Chrome(executable_path=chrome, chrome_options=chromeOptions)
+
+# login
 driver.get("https://www.americanexpress.com/hk/")
 
 username = driver.find_element_by_id("login-user")
 username.click()
-username.send_keys(credential['americanExpress']['username'])
+username.send_keys(credential["AmericanExpress"]["username"])
 
 password = driver.find_element_by_id("login-password")
 password.click()
-password.send_keys(credential['americanExpress']['password'])
+password.send_keys(credential["AmericanExpress"]["password"])
 
 driver.find_element_by_id("login-submit").click()
 
+# get the latest pdf
 driver.get("https://global.americanexpress.com/myca/intl/pdfstmt/japa/statementImageInfo.do?BPIndex=0&request_type=&Face=en_HK&sorted_index=1")
+
+
+soup = BeautifulSoup(driver.page_source, "html.parser")
+links = soup.find_all("a", {"class": "link"})
+
+today = datetime.now()
+for link in links:
+    linkDate = datetime.strptime(link.contents[0], "%d %B %Y")
+    if linkDate.year == today.year and linkDate.month == today.month - 1:
+        driver.execute_script(link["href"])
